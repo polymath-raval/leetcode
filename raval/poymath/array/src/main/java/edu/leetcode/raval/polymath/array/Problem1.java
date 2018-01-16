b429f569954a94c5644ab403fa48c33479631fab
Nikunj Raval <polymath.raval@gmail.com>
2018-01-16 09:55:37 +0530

    So it begins

---
+++ raval/poymath/array/src/main/java/edu/leetcode/raval/polymath/array/Problem1.java
@@ -0,0 +1,15 @@
+package edu.leetcode.raval.polymath.array;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+public class Problem1 {
+
+  public int removeDuplicates(int[] nums) {
+    boolean done=false;
+    do{
+      int sameIndex = -1;
+      for(){
+        
+      }
+    }while(!done);
+  }
+}
---
+++ raval/poymath/array/src/test/java/edu/leetcode/raval/polymath/array/Problem1Test.java
@@ -0,0 +1,11 @@
+package edu.leetcode.raval.polymath.array;
+
+import org.junit.Test;
+
+public class Problem1Test {
+  Problem1 solution = new Problem1();
+  @Test
+  public void test1(){
+    solution.removeDuplicates(new int[]{1,2,3,3,3,4,5,6});
+  }
+}
